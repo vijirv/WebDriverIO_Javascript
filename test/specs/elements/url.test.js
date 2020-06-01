506a4da5afbeb63d4695690166afee5eccbea2c8
unknown <vijirv@yahoo.com>
2020-06-01 22:30:06 +0100

    url.js

---
+++ test/specs/elements/url.test.js
@@ -0,0 +1,7 @@
+describe("amazon page element actions", () =>{
+
+    it("check page url", () =>{
+        browser.url('/'); //base url from config
+    })
+
+})
---
+++ url.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+        qa: 'https://www.google.com',
+        dev: 'https://www.amazon.com/',
+        stage: 'https://en-gb.facebook.com/',
+        prod: 'https://www.bbc.co.uk/'
+
+}
